e8dac15fb62763b93090a5ee7e34059acd9db806
Emily Halina <ehalina@ualberta.ca>
2023-10-24 16:46:50 -0600

    bugfixes and initial implementation of test game

--- tetris_modalities.py
+++ tetris_modalities.py
@@ -1,6 +1,17 @@
+"""
+Written with reference to user_engine.py script in tetrisRL repo
+"""
+
 from pathlib import Path
 from omegaconf import OmegaConf
-# import tetrisRL
+
+# added imports for tetrisRL
+import curses
+import numpy as np
+import os
+from engine import TetrisEngine
+
+import time
 
 def play_game_demonstration(speed, duration):
 
@@ -19,31 +30,82 @@ def play_game_warmup(speed, duration):
 
     # Run a normal tetris session for N minutes at speed <speed>
     # No agent is learning anything from this.
-    # Reset the environment
-    '''
-    env = tetrisrl.engine.TetrisEngine
+    stdscr = curses.initscr()
+    width, height = 10, 20 # standard tetris friends rules
+    env = TetrisEngine(width, height)
 
-    obs = env.clear()
+    # init timer
+    start = time.time()
+    curr = time.time()
 
-    while True:
-        # Get an action from a theoretical AI agent
-        action = agent(obs)
+    # init our game
+    initialize_game(speed, stdscr)
+    stdscr.clear()
+    env.clear()
 
-        # Sim step takes action and returns results
-        obs, reward, done = env.step(action)
+    # init render
+    stdscr.addstr(str(env))
 
-        # Done when game is lost
-        if done:
-            break
-    print("tetris warmup")
-    '''
-    pass
+    # store information about playthrough
+    db = []
+    reward_sum = 0
+
+    while curr - start < duration:
+        curr = time.time()
+        action = 6
+        key = stdscr.getch()
+
+        if key == -1: # No key pressed
+            action = 6
+        elif key == ord('a'): # Shift left
+            action = 0
+        elif key == ord('d'): # Shift right
+            action = 1
+        elif key == ord('w'): # Hard drop
+            action = 2
+        elif key == ord('s'): # Soft drop
+            action = 3
+        elif key == ord('q'): # Rotate left
+            action = 4
+        elif key == ord('e'): # Rotate right
+            action = 5
+
+        # Game step
+        state, reward, done = env.step(action)
+        reward_sum += reward
+        db.append(np.array((state,reward,done,action)))
+
+        # Render
+        stdscr.clear()
+        stdscr.addstr(str(env))
+
+        # can remove these if desired
+        stdscr.addstr('\ncumulative reward: ' + str(reward_sum))
+        stdscr.addstr('\ntime: ' + str(round(curr - start, 2)) + ' seconds')
+    
+    terminate_game(stdscr)
+
+    return
+
+def initialize_game(speed, stdscr):
+    # Don't display user input
+    curses.noecho()
+    # React to keys without pressing enter (700ms delay)
+    curses.halfdelay(speed)
+    # Enumerate keys
+    stdscr.keypad(True)
+
+def terminate_game(stdscr):
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
 
 def run_all_experiments(log_path, conf, experiment_settings):
 
-    slow_speed = conf.slow_speed
-    normal_speed = conf.slow_speed
-    fast_speed = conf.fast_speed
+    slow_speed = conf.speeds.slow
+    normal_speed = conf.speeds.normal
+    fast_speed = conf.speeds.fast
 
     # Game duration in minutes
     game_duration = conf.game_length
@@ -95,7 +157,9 @@ def __parse_args():
 
     args = parser.parse_known_args()
 
-    return Path(args.log_dir), args.participant_id
+    print(type(args[0].log_dir), type(args[0].participant_id))
+    
+    return Path(args[0].log_dir), args[0].participant_id
 
 if __name__ == "__main__":
 
@@ -103,7 +167,7 @@ if __name__ == "__main__":
 
     conf, participant_settings = __read_conf()
 
-    experiment_settings = participant_settings[str(participant_id)]
+    experiment_settings = participant_settings[participant_id]
 
     print(f"This participant's settings: {experiment_settings}")
 
